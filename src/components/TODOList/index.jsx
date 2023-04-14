1ba818cceecf6df5e9ef912bfae2660c76f7a06b
parkgain <pgi082247@gmail.com>
2023-04-14 20:59:01 +0900

    feat : 할일 완료하면 체크 해서 서버로 보내는 기능 구현 완료

--- src/components/TODOList/index.jsx
+++ src/components/TODOList/index.jsx
@@ -10,11 +10,15 @@ import {
 } from "./styles";
 import { getTodos } from "apis/todo";
 import { addTodo } from "apis/todo";
+import { deleteTodo } from "apis/todo";
+import { updateTodo } from "apis/todo";
 
 const TODOList = () => {
-  const [token, setToken] = useState();
+  const [token, setToken] = useState("");
   const [list, setList] = useState([]);
   const [todo, setTodo] = useState("");
+  const [update, setUpdate] = useState();
+  const [updateValue, setUpdateValue] = useState(list.todo);
 
   useEffect(() => {
     setToken(localStorage.getItem("token"));
@@ -39,13 +43,44 @@ const TODOList = () => {
     e.preventDefault();
     setTodo(e.target.value);
   };
+  const handleUpdateValue = (e) => {
+    e.preventDefault();
+    setUpdateValue(e.target.value);
+  };
+
+  const updateTODO = (id) => {
+    const updateTODOValue = { todo: updateValue, isCompleted: false };
+    updateTodo(id, updateTODOValue);
+    setUpdateValue("");
+    setUpdate();
+  };
+
+  const deleteTODO = (id) => {
+    deleteTodo(id);
+    const removeArr = [...list].filter((list) => list.id !== id);
+    setList(removeArr);
+  };
+
+  const completeTodo = (id) => {
+    const isCompleted = list.map((list) => {
+      if (list.id === id) {
+        list.isCompleted = !list.isCompleted;
+        updateTodo(id, {
+          todo: `${list.todo}`,
+          isCompleted: list.isCompleted,
+        });
+      }
+      return list;
+    });
+    setList(isCompleted);
+  };
 
   return (
     <Container>
       <InputContainer>
         <Input
           type="text"
-          placeholder="Enter task..."
+          placeholder="할일을 입력하세요..."
           data-testid="new-todo-input"
           value={todo}
           onChange={onchangeTODO}
@@ -58,15 +93,57 @@ const TODOList = () => {
         {list?.map((e, i) => (
           <ListItem key={e}>
             <label>
-              <input type="checkbox" value={list[i].isCompleted} />
-              <span>{list[i].todo}</span>
-              <UpdateDeleteButton data-testid="modify-button">
-                수정
-              </UpdateDeleteButton>
-              <UpdateDeleteButton data-testid="delete-button">
-                삭제
-              </UpdateDeleteButton>
+              <input
+                type="checkbox"
+                checked={list[i].isCompleted}
+                onClick={() => completeTodo(list[i].id)}
+              />
+              {update == i ? (
+                <input
+                  defaultValue={list[i].todo}
+                  value={updateValue}
+                  onChange={handleUpdateValue}
+                />
+              ) : (
+                <span>{list[i].todo}</span>
+              )}
             </label>
+            {update == i ? (
+              <div>
+                <UpdateDeleteButton
+                  data-testid="modify-button"
+                  onClick={() => {
+                    setUpdate(i);
+                    updateTODO(list[i].id);
+                  }}
+                >
+                  제출
+                </UpdateDeleteButton>
+                <UpdateDeleteButton
+                  data-testid="delete-button"
+                  onClick={() => setUpdate()}
+                >
+                  취소
+                </UpdateDeleteButton>
+              </div>
+            ) : (
+              <div>
+                <UpdateDeleteButton
+                  data-testid="modify-button"
+                  onClick={() => {
+                    setUpdate(i);
+                  }}
+                >
+                  수정
+                </UpdateDeleteButton>
+                <UpdateDeleteButton
+                  data-testid="delete-button"
+                  onClick={() => deleteTODO(list[i].id)}
+                >
+                  삭제
+                </UpdateDeleteButton>
+              </div>
+            )}
           </ListItem>
         ))}
       </ListContainer>
